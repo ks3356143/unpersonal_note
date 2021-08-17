a06db70183c452f948c07a2fc2675aa9f5f777d1
Chen junyi <314298729@qq.com>
2021-08-17 15:02:18 +0800

    模块化

---
+++ vuebegin/第三篇章：模块化/02-ES6模块化/aaa.js
@@ -0,0 +1,10 @@
+var name = "小明"
+var age = 18
+var flag = true
+
+function sum(num1,num2){
+    return num1+num2;
+}
+
+export default name
+export {sum,flag}
---
+++ vuebegin/第三篇章：模块化/02-ES6模块化/bbb.js
@@ -0,0 +1,8 @@
+var name2 = "小红"
+var age = 21
+var flag2 = true
+
+import name from './aaa.js';
+import {sum} from './aaa.js';
+console.log(name);
+console.log(sum(17,34));
