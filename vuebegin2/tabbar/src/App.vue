f89c96f98cbd3ea396308778b9ad5bfeb4229e42
chen junyi <314298729@qq.com>
2021-08-20 00:25:34 +0800

    new bar

--- vuebegin2/tabbar/src/App.vue
+++ vuebegin2/tabbar/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <div id="tab-bar">
-      <div>首页</div>
-      <div>分类</div>
-      <div>购物车</div>
-      <div>我的</div>
+      <div class="tab-bar-item">首页</div>
+      <div class="tab-bar-item">分类</div>
+      <div class="tab-bar-item">购物车</div>
+      <div class="tab-bar-item">我的</div>
     </div>
   </div>
 </template>
@@ -17,7 +17,24 @@ export default {
 </script>
 
 <style>
-@import './assets/css/base.css'
+@import './assets/css/base.css';
 
+#tab-bar {
+  display: flex;
+  background-color: #f6f6f6;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom:0;
+  box-shadow: 0px -1px 1px rgba(100,100,100,0.1);
+}
+
+.tab-bar-item {
+  flex:1; 
+  /* flex的均等分 */
+  text-align: center;
+  height: 49px;
+  /* 移动端开发一般49px */
+}
 
 </style>
