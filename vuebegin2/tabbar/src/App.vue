916eceefaeea76d23687aa5563c01f056b23c318
chen junyi <314298729@qq.com>
2021-08-20 00:12:07 +0800

    bar

---
+++ vuebegin2/tabbar/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div id="tab-bar">
+      <div>首页</div>
+      <div>分类</div>
+      <div>购物车</div>
+      <div>我的</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  component:{}
+}
+</script>
+
+<style>
+@import './assets/css/base.css'
+
+
+</style>
